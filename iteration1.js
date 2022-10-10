cc4b38ce334fbbfd0241bc3b403ce5adba4568f3
Kind <alvaromoreno21197@gmail.com>
2022-10-10 17:16:39 +0200

    iteration#1

---
+++ iteration1.js
@@ -0,0 +1,16 @@
+button = document.querySelector('.showme');
+console.log(button);
+
+h1pillado = document.querySelector('#pillado');
+console.log(h1pillado);
+
+allPs = document.querySelectorAll('p');
+console.log(allPs);
+
+allPokemons = document.querySelectorAll('.pokemon');
+console.log(allPokemons);
+
+testsFunctions = document.querySelectorAll('[data-function="testMe"]');
+console.log(testsFunctions);
+
+console.log(testsFunctions[2]);
